33f7fa8df185d33ab938e63a7e1d6e0b36501c99
Abel <abel.ze.normand@gmail.com>
2016-05-07 00:07:11 +0600

    fixed bugs with const_iterator

--- main.cpp
+++ main.cpp
@@ -333,9 +333,9 @@ public:
 	public:
 		typedef const_iterator self_type;
 		typedef int difference_type;
-		typedef T value_type;
-		typedef T& reference;
-		typedef T* pointer;
+		typedef const T value_type;
+		typedef const T& reference;
+		typedef const T* pointer;
 		typedef std::random_access_iterator_tag iterator_category;
 
 		pointer ptr;
